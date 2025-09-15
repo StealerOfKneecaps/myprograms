41ad6462f561eec42fe0987550c4dc1418dd5aaa
Lucas Zhao <zhao.lucas@outlook.com>
2025-09-15 10:52:25 -0400

    guys we did an eepy

---
+++ p1.py
@@ -0,0 +1,13 @@
+# Sup fam, this is my not first python program. It is so lit *dabs* 
+# created 15/9/2025
+# learn python 
+# editor: skibidi ohio rizzler
+print("THE PALE KING DID NOTHING WRONG") # this is a hollow knight reference btw 
+paleKing = input("guys did paleking do anything wrong\n")
+if paleKing=="yes" or "yea" or "yeah" or "ye" or "y":
+    print("well well well guess who's being SKIBIDI RIZZED tonight")
+else:
+    print("yea cool")
+age = input("How old is gng heartbreak emoji rosewilt emoji\n")
+name = input("What is gngs name heartbreak emoji rosewilt emoji\n")
+print(f"gng is {age} and is called {name} heartbreak emoji rosewilt emoji")
