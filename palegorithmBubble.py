7ed8f7633a2be6ef1597a386ae3d99d52fd0fdd8
StealerOfKneecaps <zhao.lucas@outlook.com>
2025-10-27 10:08:20 -0400

    better

--- palegorithmBubble.py
+++ palegorithmBubble.py
@@ -8,3 +8,15 @@ def bubbly(notSorted):
             i+=1
     return notSorted
 print (bubbly([5, 5, 5, 3, 5, 5, 4, 5, 5, 5, 4, 1, 2]))
+
+# def bubobubobly (unSorte):
+#     swap = True
+#     while swap:
+#         swap = False
+#         for i in range (1, len(unSorte)):
+#             if unSorte[i]<unSorte[i-1]:
+#                 unSorte[i],unSorte[i-1] = unSorte[i-1],unSorte[i]
+#                 swap = True
+#     return unSorte
+# print (bubobubobly([5,4,6,3,6,5,4,2]))
+#This one is slightly better
